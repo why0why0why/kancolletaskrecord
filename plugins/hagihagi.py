49b69ab905e5d4fae640f46827df7f6e5abcf779
NoNo <qwedfghjkrtyuiopas@gmail.com>
2021-01-21 17:50:58 +0800

    Add hagi

---
+++ plugins/hagihagi.py
@@ -0,0 +1,6 @@
+﻿from nonebot import on_command, CommandSession
+
+@on_command('萩', aliases=('萩图','hagihagi'), only_to_me=False)
+async def task(session: CommandSession):
+    await session.send("[CQ:image,file=plugins\CQHTTPMirai\images\hagi.jpg]ハギハギ♡ドキドキ")
+    session.finish()
